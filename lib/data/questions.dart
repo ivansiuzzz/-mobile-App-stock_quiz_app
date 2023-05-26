24e23aab79f09663339d3d5237abcc68f7c6dbf3
ivansiuzzzzz <ivan.siu@icloud.com>
2023-05-26 19:39:34 +0800

    create question modal

---
+++ lib/data/questions.dart
@@ -0,0 +1,43 @@
+import 'package:quiz_stock/modals/QuizQuestion.dart';
+
+const questions = [
+  QuizQuestion(
+    'Which type of investment carries the highest risk?',
+    ['A. Stocks', 'B. Bonds', 'C. Real estate', 'D. Gold'],
+  ),
+  QuizQuestion(
+    'Which indicator measures the overall performance of the stock market?',
+    [
+      'A. Foreign exchange rates',
+      'B. GDP',
+      'C. Stock price index',
+      'D. Inflation rate'
+    ],
+  ),
+  QuizQuestion(
+    'Which investment tool is best suited for long-term savings',
+    ['A. Stocks', 'B. Bonds', 'C. Real estate', 'D. Gold'],
+  ),
+  QuizQuestion(
+    ' What is an investment portfolio',
+    [
+      'A. All the stocks an investor owns',
+      'B. All the assets an investor diversifies into',
+      'C. All the bonds an investor owns',
+      'D. All the real estate an investor owns'
+    ],
+  ),
+  QuizQuestion(
+    'Which type of investment carries the highest risk?',
+    ['A. Stocks', 'B. Bonds', 'C. Real estate', 'D. Gold'],
+  ),
+  QuizQuestion(
+    'Which type of investment risk is considered a market risk?',
+    [
+      'A. Business operation risk',
+      'B. Interest rate risk',
+      'C. Currency risk',
+      'D. Geopolitical risk'
+    ],
+  ),
+];
