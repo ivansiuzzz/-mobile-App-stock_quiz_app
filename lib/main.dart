f60fc0f039a88f79a530d361ccd831b93a8322ba
ivansiuzzzzz <ivan.siu@icloud.com>
2023-05-26 17:51:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_stock/pages/startPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.amber,
+          appBar: AppBar(
+            title: Text(
+              "Learning Option is important",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue,
+          ),
+          body: StartPage(),
+        ));
+  }
+}
---
+++ lib/pages/startPage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/1753732.png',
+          width: 300,
+        ),
+        Container(
+          child: Text(
+            'Learning stock market!',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+        ),
+        ElevatedButton(onPressed: () {}, child: Text("starting amazing way"))
+      ],
+    ));
+  }
+}
